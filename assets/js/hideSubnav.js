6c6e9d2c7b0f406d14c2cc0864b6f211fbdc02be
Phuc To <totrongphuc239@gmail.com>
2023-04-05 22:13:46 +0700

    Fixed Bug

--- assets/js/hideSubnav.js
+++ assets/js/hideSubnav.js
@@ -1,14 +1,15 @@
 var moreBtn = document.querySelector('.moreBtn')
 var subnav = document.querySelector('.subnav')
 
-
-moreBtn.onclick = function(){
-    const computedStyles = window.getComputedStyle(subnav) 
-    const subnavPosition = computedStyles.position
-    var Opened  = subnavPosition === 'relative'
-    if (Opened){
-        subnav.style.position = 'absolute'
-    } else {
-        subnav.style.position = 'relative'
-    }
+if (window.matchMedia("(max-width: 740px)").matches) {
+    moreBtn.onclick = function(){
+        const computedStyles = window.getComputedStyle(subnav) 
+        const subnavPosition = computedStyles.position
+        var Opened  = subnavPosition === 'relative'
+        if (Opened){
+            subnav.style.position = 'absolute'
+        } else {
+            subnav.style.position = 'relative'
+        }
+    } 
 }
